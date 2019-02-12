7d22337b36aba085380830c962f521144ac1d529
skusVV <skus1992@gmail.com>
2019-02-12 21:59:20 +0200

    working with visualize category

--- front/src/components/categoryVisualize/categoryVisualize.component.ts
+++ front/src/components/categoryVisualize/categoryVisualize.component.ts
@@ -2,10 +2,15 @@ import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {IPayment} from '../../store/reducers/data.reducer';
 
+const VISUALISE_BY_CURRENCY = 'byCurrency';
+const VISUALISE_BY_DATE = 'byDate';
+
 interface ICategoryVisualize {
   title: string;
   payments: IPayment[];
 }
+
+type IActiveTab = 'byDate' | 'byCurrency';
 
 @Component({
   selector: 'app-category-visualize',
@@ -13,11 +18,24 @@ interface ICategoryVisualize {
   styleUrls: ['./categoryVisualize.component.less']
 })
 export class CategoryVisualizeComponent {
+  activeTab: IActiveTab = VISUALISE_BY_CURRENCY;
 
   constructor(public dialogRef: MatDialogRef<CategoryVisualizeComponent>,
               @Inject(MAT_DIALOG_DATA) public data: ICategoryVisualize) {}
 
-  onNoClick() {
+  get isByCurrencyActive(): boolean {
+    return this.activeTab === VISUALISE_BY_CURRENCY;
+  }
+
+  get isByDateActive(): boolean {
+    return this.activeTab === VISUALISE_BY_DATE;
+  }
+
+  close() {
     this.dialogRef.close();
+  }
+
+  setActiveTab(activeTab: IActiveTab) {
+    this.activeTab = activeTab;
   }
 }
